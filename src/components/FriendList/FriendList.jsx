6bc89f430abbc8e4b8eb3218922d8603c8e31207
MagdalenaBernat <magdalena.bernat@onet.pl>
2024-07-13 13:39:48 +0200

    update

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,28 +1,46 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import './friends.json'
 import css from './FriendList.module.css'
 
-const friendsData = {
-    avatar: "",
-    name: "",
-    isOnline: true
-} 
-
-const FriendListItem = ({ avatar, name, isOnline }) => {
+const friendsData = [
+    {
+        avatar: "",
+        name: "Mango",
+        isOnline: true
+    },
+    {
+        avatar: "",
+        name: "Kiwi",
+        isOnline: false
+    },
+    {
+        avatar: "",
+        name: "Ajax",
+        isOnline: true
+    },
+    {
+        avatar: "",
+        name: "Jay",
+        isOnline: true
+    },
+    {
+        avatar: "",
+        name: "Poly",
+        isOnline: false
+    } 
+]
+const FriendListItem = () => {
     return (
-        <>
-            <ul className={css.friendList}>
-                friendsData.map((friend, index) => {
-                    <li className={css.item} key={index}>
-                        <span className={css.isOnline}>{friendsData.isOnline}</span>
-                        <img className={css.avatar} src={friendsData.avatar} alt={`Avatar of ${friend.name}`} width="50" />
-                        <p className={css.name}>{friend.name}</p>
-                    </li>
-                })
-            </ul>
-        </>
-    )
+        <ul>
+            {friendsData.map((friend, index) => (
+                <li className={css.item} key={index}>
+                    <span className={`status ${friend.isOnline ? 'online' : 'offline'}`}></span>
+                    <img className={css.avatar} src={friend.avatar} alt={`Avatar of ${friend.name}`} width="50" />
+                    <p className={css.name}>{friend.name}</p>
+                </li>
+            ))}
+        </ul>
+    );
 }
 
 FriendListItem.propTypes = {
